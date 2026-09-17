ed6ff7a9a0e103f68921fa77b312c46624087d8f
Maverick <debashc105@gmail.com>
2026-09-17 23:55:45 +0530

    working on security

--- src/main/java/com/example/cloneproject/config/SecurityConfig.java
+++ src/main/java/com/example/cloneproject/config/SecurityConfig.java
@@ -1,11 +1,18 @@
 package com.example.cloneproject.config;
+import com.example.cloneproject.service.security.JwtAuthenticationFilter;
+import jakarta.persistence.Table;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 
-@Configurable
+@Configuration
 public class SecurityConfig {
+
+    JwtAuthenticationFilter jwtAuthenticationFilter;
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter){
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
